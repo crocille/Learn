3f58e64608e31ea1f9166594713a5ca704768cbe
Alexandr Dikarev <admin@cfif31.ru>
2022-01-31 10:37:33 +0400

    Add project files.

---
+++ LearnApp/Pages/AdminServiceListPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LearnApp.Pages
+{
+    /// <summary>
+    /// Interaction logic for AdminServiceListPage.xaml
+    /// </summary>
+    public partial class AdminServiceListPage : Page
+    {
+        public AdminServiceListPage()
+        {
+            InitializeComponent();
+        }
+
+        private void BtRemoveClick(object sender, RoutedEventArgs e)
+        {
+            if(MessageBox.Show("Вы действительно хотите удалить услугу?", "Удаление услуги", MessageBoxButton.YesNo)
+                == MessageBoxResult.Yes)
+            {
+                MessageBox.Show("Функционал в разработке");
+            }
+        }
+
+        private void BtEditClick(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new ServiceAddPage());
+        }
+
+        private void BtAddClick(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new ServiceAddPage());
+        }
+
+        private void BtOrderListClick(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new OrderListPage());
+        }
+    }
+}
---
+++ LearnApp/Pages/StartPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LearnApp.Pages
+{
+    /// <summary>
+    /// Interaction logic for StartPage.xaml
+    /// </summary>
+    public partial class StartPage : Page
+    {
+        public StartPage()
+        {
+            InitializeComponent();
+        }
+
+        private void BtAdminClick(object sender, RoutedEventArgs e)
+        {
+            if (TbAdminCode.Text == "0000")
+            {
+                NavigationService.Navigate(new AdminServiceListPage());
+            }
+            else {
+                MessageBox.Show("Код доступа к странице администратора неверный!");
+            }
+        }
+
+        private void BtBaseModeClick(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new ServiceListPage());
+        }
+    }
+}
